656ae6e95d00e02e80f9182e6c0bb8362ef70f1a
Shivansh Vishwakarma <eliteprincesv@gmail.com>
2026-07-29 05:14:03 +0530

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(PROJECT_ROOT))
+
 import streamlit as st
 from src.predict import predict_salary
 
